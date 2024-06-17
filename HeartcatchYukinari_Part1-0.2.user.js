911bec8fef680ffc04dbe03f32ea04b3634438a7
Da1eth <111392023+Da1eth@users.noreply.github.com>
2024-06-17 22:24:04 +0900

    first upload

---
+++ HeartcatchYukinari_Part1-0.2.user.js
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name         HeartcatchYukinari_Part1
+// @namespace    https://github.com/Da1eth
+// @version      0.2
+// @description  maybe good script with Tunaground
+// @author       Daleth
+// @match        https://bbs.tunaground.net/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.head.append(Object.assign(document.createElement('style'), {
+        textContent: `
+            ruby {
+                position: relative;
+                display: inline-block;
+            }
+            rt {
+                position: absolute;
+                display: block;
+                width: 100%;
+                top: -1em;
+                text-align: center;
+            }
+        `
+    }));
+
+    const nctToChar = (text) => text.replace(/&#(x?)([0-9a-fA-F]+);/gm, (_, isHex, match) => String.fromCodePoint(parseInt(match, isHex ? 16 : 10)));
+
+    const handleElementInput = (evt) => {
+        evt.target.value = nctToChar(evt.target.value);
+    };
+
+    const handleInput = (el) => {
+        const nextEl = el.nextElementSibling;
+        el.value.split('.').includes('nct') ?
+            (nextEl.value = nctToChar(nextEl.value), nextEl.addEventListener('input', handleElementInput)) :
+        nextEl.removeEventListener('input', handleElementInput);
+    };
+
+    document.querySelectorAll('.post_form_console').forEach(el => {
+        el.addEventListener('input', () => handleInput(el));
+        el.dispatchEvent(new Event('input'));
+    });
+
+    if (window.location.href.includes('mthread.php')) {
+        window.onload = () => {
+            const addButton = (text, action) => {
+                const btn = document.createElement('button');
+                btn.textContent = text;
+                btn.classList.add('button_default');
+                btn.onclick = () => {
+                    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+                    checkboxes.forEach((c, index) => {
+                        index !== 0 ? c.checked = action : null;
+                    });
+                };
+                document.querySelector('.manage_button_set').appendChild(btn);
+            };
+
+            addButton('Check All', true);
+            addButton('Uncheck All', false);
+        };
+    }
+
+    const addSee1_50 = () => {
+        const match = window.location.href.match(/trace\.php\/([^\/]+)\/(\d+)/);
+        if (match) {
+            const ul = document.querySelector('nav ul');
+            if (ul) {
+                const newLi = document.createElement('li');
+                newLi.innerHTML = `<a href="/trace.php/${match[1]}/${match[2]}/1/50">1-50 보기</a>`;
+                ul.insertBefore(newLi, ul.children[5]);
+            }
+        }
+    };
+
+    addSee1_50();
+
+})();
---
+++ HeartcatchYukinari_Part4-0.1.user.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         HeartcatchYukinari_Part4
+// @namespace    https://github.com/Da1eth
+// @version      0.1
+// @description  maybe good script with Tunaground
+// @author       Daleth
+// @match        https://bbs.tunaground.net/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const createPopup = () => {
+        const popup = document.createElement('div');
+        popup.className = 'popup';
+        Object.assign(popup.style, {
+            position: 'absolute',
+            border: '1px solid #cccccc',
+            backgroundColor: '#fafafa',
+            padding: '10px',
+        });
+        document.body.appendChild(popup);
+
+        popup.addEventListener('mouseover', () => clearTimeout(popup.hideTimeout));
+        popup.addEventListener('mouseout', () => {
+            popup.hideTimeout = setTimeout(hidePopup, 200);
+        });
+
+        return popup;
+    };
+
+    const popup = createPopup();
+
+    const showPopup = (content, x, y) => {
+        popup.innerHTML = content;
+
+        popup.querySelectorAll('p').forEach(el => {
+            el.style.margin = '0';
+        });
+        popup.querySelectorAll('*').forEach(el => {
+            const fontSize = parseFloat(window.getComputedStyle(el).fontSize);
+            if (fontSize > 16) {
+                el.style.fontSize = '16px';
+            }
+        });
+        popup.querySelectorAll('.response_create_date').forEach(el => {
+            el.style.fontSize = '0.7em';
+        });
+        popup.querySelectorAll('.button_default.clr_copy').forEach(button => button.remove());
+
+        popup.style.display = 'block';
+        Object.assign(popup.style, {
+            left: `${x + 10}px`,
+            top: `${y - 10 - popup.offsetHeight}px`
+        });
+    };
+
+    const hidePopup = () => {
+        popup.style.display = 'none';
+    };
+
+    document.querySelectorAll('a[href]').forEach(link => {
+        link.addEventListener('mouseover', event => {
+            const linkText = link.textContent;
+            const match = linkText.match(/>>(\d+)/)
+                || (linkText.split('>').length === 3 && linkText.split('>')[2].trim().match(/(\d+)/));
+            if (match && link.href.includes('#response_')) {
+                const responseElement = document.querySelector(`div[id$="_${match[1]}"]`);
+                if (responseElement) {
+                    showPopup(responseElement.innerHTML, event.pageX, event.pageY);
+                }
+            }
+        });
+
+        link.addEventListener('mouseout', () => {
+            popup.hideTimeout = setTimeout(hidePopup, 200);
+        });
+    });
+})();
