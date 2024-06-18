e7fd8578e0905613d1d58da1557c325e26a513c9
Da1eth <111392023+Da1eth@users.noreply.github.com>
2024-06-18 22:10:56 +0900

    rename file

--- HeartcatchYukinari_Part4.js
+++ HeartcatchYukinari_Part4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HeartcatchYukinari_Part4
 // @namespace    https://github.com/Da1eth
-// @version      0.1
+// @version      0.101
 // @description  maybe good script with Tunaground
 // @author       Daleth
 // @match        https://bbs.tunaground.net/*
