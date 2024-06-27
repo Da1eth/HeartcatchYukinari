8cdcd79bfc9dc2a1310fbf596522df191997beeb
Da1eth <111392023+Da1eth@users.noreply.github.com>
2024-06-28 00:44:58 +0900

    version miss fix

--- HeartcatchYukinari_Part6.js
+++ HeartcatchYukinari_Part6.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HeartcatchYukinari_Part6
 // @namespace    https://github.com/Da1eth
-// @version      0.1.3
+// @version      0.1.4
 // @description  maybe good script with Tunaground
 // @author       Daleth
 // @match        https://bbs.tunaground.net/*
