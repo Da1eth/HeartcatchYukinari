e995ac246a4e916dce4b3112d2b3fd875f5d8774
Da1eth <111392023+Da1eth@users.noreply.github.com>
2026-03-05 22:45:48 +0900

    new-chamchijs

--- Shingetsu_Main.js
+++ Shingetsu_Main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shingetsu_Main
 // @namespace    https://github.com/Da1eth
-// @version      1.0.2
+// @version      1.0.3
 // @description  maybe good script with Tunaground
 // @author       Daleth
 // @match        https://bbs2.tunaground.net/*
@@ -9,50 +9,28 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+ (function() {
     'use strict';
 
     document.addEventListener('keydown', event => {
         const { target, key } = event;
         const isTabKey = key === 'Tab';
         const isEnterKey = key === 'Enter';
-        const isTextarea = target.matches('textarea[name="content"]');
-        const isInput = target.matches('input');
+        const isTextarea = target.matches('textarea');
         const isSubmitButton = target.matches('button[type="submit"]');
+        const form = target.closest('form');
 
         isTabKey && isTextarea ? (
             event.preventDefault(),
-            target.closest('form')?.querySelector('button')?.focus()
-        ) : isEnterKey && isInput ? (
-            event.preventDefault(),
-            target.closest('form')?.querySelectorAll('input, textarea, button, select, a[href]')
-            .item(Array.from(target.closest('form').querySelectorAll('input, textarea, button, select, a[href]')).indexOf(target) + 1)
-            ?.focus()
-        ) : isEnterKey && isSubmitButton ? (
-            setTimeout(() => {
-                target.closest('form')?.querySelector('textarea[name="content"]')?.focus();
-            }, 250)
+            form?.querySelector('button[type="submit"]')?.focus()
+        ) : isEnterKey ? (
+            !isTextarea && !isSubmitButton ? (
+                event.preventDefault(),
+                ((elems) => {
+                    const nextIdx = elems.indexOf(target) + 1;
+                    elems[nextIdx]?.focus();
+                })(Array.from(form?.querySelectorAll('input, textarea, button, select, a[href]') || []))
+            ) : null
         ) : null;
     });
-
-    const adjustHeight = textarea => requestAnimationFrame(() => {
-        const { lineHeight, fontSize } = getComputedStyle(textarea);
-        const normalHeight = lineHeight === 'normal' ? parseFloat(fontSize) * 1.155 : parseFloat(lineHeight);
-        textarea.style.height = `${textarea.value.split('\n').length * normalHeight}px`;
-    });
-
-    const heightObserver = () => new MutationObserver(mutations =>
-                                                      mutations.forEach(m => m.target.matches('textarea[name="content"]') && adjustHeight(m.target))
-                                                     ).observe(document.body, { childList: true, subtree: true, attributes: true, attributeFilter: ['style'] });
-
-    window.addEventListener('load', () => {
-        document.querySelectorAll('textarea[name="content"]').forEach(textarea => {
-            textarea.style.whiteSpace = 'nowrap';
-            adjustHeight(textarea);
-        });
-        heightObserver();
-    });
-
-    document.addEventListener('input', e => e.target.matches('textarea[name="content"]') && adjustHeight(e.target));
-
 })();
